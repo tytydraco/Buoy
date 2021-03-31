1c04bfd5343191744dccb53a833e966d90cbeacc
Tyler Nijmeh <tylernij@gmail.com>
2021-03-31 12:30:03 -0700

    Add quick settings tiles
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/buoy/services/TileLightService.kt
@@ -0,0 +1,19 @@
+package com.draco.buoy.services
+
+import android.service.quicksettings.TileService
+import com.draco.buoy.repositories.profiles.BatterySaverConstantsConfigProfiles
+import com.draco.buoy.utils.BatterySaverManager
+
+class TileLightService : TileService() {
+    private lateinit var batterySaverManager: BatterySaverManager
+
+    override fun onCreate() {
+        super.onCreate()
+        batterySaverManager = BatterySaverManager(contentResolver)
+    }
+
+    override fun onClick() {
+        super.onClick()
+        batterySaverManager.apply(BatterySaverConstantsConfigProfiles.LIGHT)
+    }
+}
---
+++ app/src/main/java/com/draco/buoy/services/TileResetService.kt
@@ -0,0 +1,18 @@
+package com.draco.buoy.services
+
+import android.service.quicksettings.TileService
+import com.draco.buoy.utils.BatterySaverManager
+
+class TileResetService : TileService() {
+    private lateinit var batterySaverManager: BatterySaverManager
+
+    override fun onCreate() {
+        super.onCreate()
+        batterySaverManager = BatterySaverManager(contentResolver)
+    }
+
+    override fun onClick() {
+        super.onClick()
+        batterySaverManager.reset()
+    }
+}
