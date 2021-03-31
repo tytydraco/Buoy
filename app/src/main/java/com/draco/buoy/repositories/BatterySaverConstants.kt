8e8225754430376bebc8d9993da2a3fd7bfa23ad
Tyler Nijmeh <tylernij@gmail.com>
2021-03-30 19:59:11 -0700

    Import battery saver constants
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/buoy/repositories/BatterySaverConstants.kt
@@ -0,0 +1,22 @@
+package com.draco.buoy.repositories
+
+object BatterySaverConstants {
+    const val ADVERTISE_IS_ENABLED =        "advertise_is_enabled"
+    const val DATASAVER_DISABLED =          "datasaver_disabled"
+    const val ENABLE_NIGHT_MODE =           "enable_night_mode"
+    const val LAUNCH_BOOST_DISABLED =       "launch_boost_disabled"
+    const val VIBRATION_DISABLED =          "vibration_disabled"
+    const val ANIMATION_DISABLED =          "animation_disabled"
+    const val SOUNDTRIGGER_DISABLED =       "soundtrigger_disabled"
+    const val FULLBACKUP_DEFERRED =         "fullbackup_deferred"
+    const val KEYVALUEBACKUP_DEFERRED =     "keyvaluebackup_deferred"
+    const val FIREWALL_DISABLED =           "firewall_disabled"
+    const val GPS_MODE =                    "gps_mode"
+    const val ADJUST_BRIGHTNESS_DISABLED =  "adjust_brightness_disabled"
+    const val ADJUST_BRIGHTNESS_FACTOR =    "adjust_brightness_factor"
+    const val FORCE_ALL_APPS_STANDBY =      "force_all_apps_standby"
+    const val FORCE_BACKGROUND_CHECK =      "force_background_check"
+    const val OPTIONAL_SENSORS_DISABLED =   "optional_sensors_disabled"
+    const val AOD_DISABLED =                "aod_disabled"
+    const val QUICK_DOZE_ENABLED =          "quick_doze_enabled"
+}
